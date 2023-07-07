729a21d3e296f68965964eb25cc20c7204a5cc03
ivanugarte <ivanugartem@gmail.com>
2023-07-06 21:05:52 -0400

    commit jueves

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Button from "../button/Button"
 import Cards from "../cards/Cards"
 import Footer from "./footer/Footer"
 import Nav from "./nav/Nav"
@@ -8,33 +9,11 @@ import Image from 'next/image'
 
 export const Layout = ({ children }: { children: any }) => {
   return (
-    <div>
-      <Image
-        src="/img/logobv.jpg"
-        width={800}
-        height={1000}
-        alt="Workflow"
-        priority
-      />
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
-        ESPAÑOL
-      </button>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
-        INGLES
-      </button> <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
-        FRANCES
-      </button>
-      <Image
-        src="/img/fondo.jpeg"
-        width={800}
-        height={1100}
-        alt="Workflow"
-        priority
-      />
+    <div className="fondoLog">
+      <Button />
       <Whatsapp />
       <Cards />
       {children}
-
     </div>
   )
 }

7adc63297317d9652280a10d4754711f177ff13d
ivanugarte <ivanugartem@gmail.com>
2023-07-18 23:20:54 -0400

    commit martes

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -2,14 +2,17 @@ export default function Button() {
     return (
         <>
             <div className="mt-80">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
+                <button className="bg-violet-900 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full ml-4">
                     ESPAÑOL
                 </button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
+                <button className="bg-violet-900 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full ml-4">
                     INGLES
-                </button> <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full ml-4">
+                </button> <button className="bg-violet-900 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-full ml-4">
                     FRANCES
                 </button>
+ 
+
+
             </div>
         </>
     )

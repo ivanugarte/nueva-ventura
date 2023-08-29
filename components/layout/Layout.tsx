1eec7b6762bf1da079f8a7980936de3f30a61c8b
ivanugarte <ivanugartem@gmail.com>
2023-08-29 00:34:37 -0400

    ahora

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,11 +1,26 @@
 import Button from "../button/Button"
+import Nav from "./nav/Nav"
 import Whatsapp from "./whatsapp/Whatsapp"
+import Logo from "./logo/Logo" 
 
-export const Layout = () => {
+import Image from 'next/image'
+
+
+export const Layout = ( children :any) => {
   return (
-    <div className="fondoLog">
-      <Button />
+    <div >
+      <Logo />
       <Whatsapp />
+      <Nav />
+      {/* <di v className="fondoLog">sdfsdf</div> */}
+
+        <Image className="mt-40 fixed " 
+          src="/img/fondo.jpeg"
+          alt="Workflow"
+          height={1200}
+          width={1800}
+          priority
+        />
     </div>
   )
 }

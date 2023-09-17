1b1a126c8e9ae2bb0dd5fb3a828f87c1680b9f0e
ivanugarte <ivanugartem@gmail.com>
2023-09-17 15:44:31 -0300

    primera vez

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import { Layout } from "../components/layout/Layout";
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Layout children={undefined} />
-      </div>
+      <><h1>hola </h1></>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
+const {nextui} = require("@nextui-org/react");  
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
   },
+  darkMode: "class",
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    [nextui()]
   ],
 }
